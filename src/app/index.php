6f8f40f89f0804508e463a89cdf04ee97ac3ff28
waxer59 <hnboyano@gmail.com>
2024-06-08 16:48:42 +0200

    add removed code

--- src/app/index.php
+++ src/app/index.php
@@ -28,8 +28,33 @@ $phones = $phoneRepository->get_all();
 
 <body>
 
-    <div class="container">
+    <div class="container"> <?php if (empty($phones)): ?>
+            <h1>No phones available</h1>
+        <?php else:
+        foreach ($phones as $phone): ?>
+                <div class='card'>
+                    <img src='<?= $phone->get_image_url() ?>' alt='<?= $phone->get_brand() . " " . $phone->get_model() ?>'>
+                    <div class='card-body'>
+                        <div class='left-column'>
+                            <h3><?= $phone->get_brand() . " " . $phone->get_model() ?></h3>
+                            <p>Screen Size: <?= $phone->get_sreen_size_inch() ?> in</p>
+                            <p>Storage: <?= $phone->get_storage_gb() ?> GB</p>
+                            <p>OS: <?= $phone->getOs() ?></p>
+                            <p class='stars'>
+                                <?= str_repeat('★', $phone->get_ratings()) . str_repeat('☆', 5 - $phone->get_ratings()) ?></p>
+                        </div>
+                        <div class='right-column'>
+                            <p class='price'>€<?= $phone->get_price_eur() ?></p>
+                            <p class='taxes'>Taxes Included</p>
+                            <div class='button-container'>
+                                <button class='addCart' onclick='buyProduct(<?= $phone->get_id() ?>)'>Add to Cart</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; endif; ?>
     </div>
 
 </body>
+
 </html>
