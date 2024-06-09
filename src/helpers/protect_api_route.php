ccc60353fba8a27c39cc860f40eebbd01ff736af
waxer59 <hnboyano@gmail.com>
2024-06-10 01:13:40 +0200

    update protect api helper

--- src/helpers/protect_api_route.php
+++ src/helpers/protect_api_route.php
@@ -5,15 +5,15 @@ require_once __DIR__ . "/env.php";
 require_once __DIR__ . "/../repositories/user_repository.php";
 require_once __DIR__ . "/../models/db_manager.php";
 
-// if (!isset($_SESSION['user_id'])) {
-//     http_response_code(403);
-//     die;
-// }
+if (!isset($_SESSION['user_id'])) {
+    http_response_code(403);
+    die;
+}
 
 $db = new Db_Manager(env("DB_HOST"), env("DB_USER"), env("DB_PASSWORD"), env("DB_NAME"), env("DB_PORT"));
 $userRepository = new User_Repository($db);
 
-$user = $userRepository->get_by_id(7);
+$user = $userRepository->get_by_id($_SESSION['user_id']);
 
 if (!$user || !$user->get_isVerified()) {
     http_response_code(403);
